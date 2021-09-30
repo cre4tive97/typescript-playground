468ab481ca67edb126e2e0a4ef538879b68e6d6f
cre4tive97 <harryborrison5148@gmail.com>
2021-10-01 04:58:11 +0900

    union, any, unknown type

--- index.js
+++ index.js
@@ -10,3 +10,38 @@ var project = {
     days: 30,
     started: true,
 };
+// union, unknown, any
+var 회원들 = [1, "2", 3];
+var 오브젝트 = { a: "123" };
+var 이름1;
+이름1 = 123;
+이름1 = {};
+var 어쩌고 = 이름1; // any는 type무시함
+var 이름2;
+이름2 = 123;
+이름2 = [];
+// let 어쩌고2: string = 이름2; << unknown은 타입 지켜줌
+// unknown 타입은 연산 불가능
+// TS에서는 연산도 엄격하게 적용됨.
+var 나이1;
+//나이1 + 1;        에러
+var 나이2;
+나이2 + 1; //잘됨
+var 나이3;
+나이3 + 1; // 잘됨
+/*
+string 타입 +1 (허용)
+number 타입 +1 (허용)
+string | number 타입 +1 (안됨)
+*/
+var user = "kim";
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- index.ts
+++ index.ts
@@ -12,3 +12,51 @@ let project: { member: string[]; days: number; started: boolean } = {
   days: 30,
   started: true,
 };
+
+// union, unknown, any
+let 회원들: (number | string)[] = [1, "2", 3];
+let 오브젝트: { a: string | number } = { a: "123" };
+
+let 이름1: any;
+이름1 = 123;
+이름1 = {};
+
+let 어쩌고: string = 이름1; // any는 type무시함
+
+let 이름2: unknown;
+이름2 = 123;
+이름2 = [];
+
+// let 어쩌고2: string = 이름2; << unknown은 타입 지켜줌
+// unknown 타입은 연산 불가능
+
+// TS에서는 연산도 엄격하게 적용됨.
+let 나이1: string | number;
+//나이1 + 1;        에러
+let 나이2: string;
+나이2 + 1; //잘됨
+let 나이3: number;
+나이3 + 1; // 잘됨
+
+/* 
+string 타입 +1 (허용)
+number 타입 +1 (허용)
+string | number 타입 +1 (안됨)
+*/
+
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | undefined | number | boolean)[] = [user, age, married];
+
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
