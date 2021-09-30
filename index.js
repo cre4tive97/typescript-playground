e9b11677d5129496064e59ce38025d2273d6f0b5
cre4tive97 <harryborrison5148@gmail.com>
2021-10-01 04:32:52 +0900

    primitive type

---
+++ index.js
@@ -0,0 +1,12 @@
+var 이름 = "최재문";
+var 나이 = 24;
+var 출생지역 = "울산";
+var favMusic = {
+    title: "시차",
+    artist: "우원재",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,14 @@
+let 이름: string = "최재문";
+let 나이: number = 24;
+let 출생지역: string = "울산";
+
+let favMusic: { title: string; artist: string } = {
+  title: "시차",
+  artist: "우원재",
+};
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
